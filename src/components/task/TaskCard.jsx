b03956dd3fabcdec98d8cfe2749627d484418dc9
libra850 <wrench.m.s@icloud.com>
2022-02-12 00:33:20 +0900

    [add]タスク追加表示

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {TaskCardTitle} from './TaskCardTitle'
 import {TaskDeleteButton} from '../task/button/TaskDeleteButton'
 import {TaskAddInput} from '../task/input/TaskAddInput'
 import {Tasks} from './Tasks'
 
 export const TaskCard = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState([]);
   return (
     <div className='taskCard'>
       <TaskCardTitle/>
       <TaskDeleteButton/>
-      <TaskAddInput/>
-      <Tasks/>
+      <TaskAddInput
+        inputText={inputText}
+        setInputText={setInputText}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
+      <Tasks
+        inputText={inputText}
+        taskList={taskList}
+      />
     </div>
   )
 }
--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 
-export const TaskAddInput = () => {
+export const TaskAddInput = (
+  {
+    inputText,
+    setInputText,
+    taskList,
+    setTaskList,
+  }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e)
+    // カードを追加する。taskListに追加していく。
+    setTaskList([
+      ...taskList,
+      {
+        text: inputText,
+      },
+    ]);
+    console.log(...taskList);
+    setInputText("");
+  };
+  const handleChange = (e) => {
+    // inputTextに入る状態をsetで更新。consoleで確認
+    setInputText(e.target.value);
+    console.log(inputText);
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='add project' className='taskAddInput'></input>
+        <input
+          type="text"
+          placeholder='add project'
+          className='taskAddInput'
+          onChange={handleChange}
+          maxLength='20'
+          // l.21で空白にリセットしたものを毎時読み込ませる
+          value={inputText}>
+        </input>
       </form>
     </div>
   )
