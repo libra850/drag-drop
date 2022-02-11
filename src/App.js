bf09b1bc8d5ac9e8d9798b289c451827469a58b0
libra850 <wrench.m.s@icloud.com>
2022-02-11 18:39:08 +0900

    [add]heaeder&background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import { Header } from "./components/header/Header";
+
 function App() {
-  return (
-    <div></div>
-  );
+  return <div className="app"> <Header /> </div>
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Drag-Drop</h1>
+      </header>
+    </div>
+  )
+}
