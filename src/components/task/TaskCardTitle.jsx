dc39d88fdc38eedc03ef9673cbc6ddfd903a203c
libra850 <wrench.m.s@icloud.com>
2022-02-11 23:04:55 +0900

    [add]タスク追加レイアウト

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const TaskCardTitle = () => {
+  // 状態関数を定義
+  const [isClick, setIsClick] = useState(false);
+  const [inputTitle, setInputTitle] = useState("TITLE");
+  const handleClick = () => {
+    setIsClick(true);
+  };
+  const handleSubmit = (e) => {
+    // 更新を防ぎ変更を保持
+    e.preventDefault();
+    // input状態を変更
+    setIsClick(false);
+  };
+  // 変更内容を保持
+  const handleChange = (e) => {
+    setInputTitle(e.target.value);
+  };
+  // enterしなくても保存されるよう変更
+  const handleBlur = () => {
+    setIsClick(false);
+  };
   return (
-    <div>
-      <h3>TITLE</h3>
+    <div onClick={handleClick} className="taskTitleInputArea">
+      {isClick? <form onSubmit={handleSubmit}>
+                  <input className='taskTitleInput'
+                         autofocus
+                         type="text"
+                         onChange={handleChange}
+                         onBlur={handleBlur}
+                         value={inputTitle}
+                         maxLength = '10'>
+                  </input>
+                </form>
+                :
+                <h3>{inputTitle}</h3>}
     </div>
   )
 }
--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export const TaskAddInput = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e)
+  };
   return (
-    <div></div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='add project' className='taskAddInput'></input>
+      </form>
+    </div>
   )
 }
